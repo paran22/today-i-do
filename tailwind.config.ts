24342b3900a9fe37e019becfce215b7187a618c8
gaeun <knospe1@naver.com>
2023-09-01 10:11:50 +0900

    Feat: add default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{jsx,js,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#2B3A55",
+        primary: "#CE7777",
+        secondary: "#E8C4C4",
+        textColor: "#F2E5E5",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
