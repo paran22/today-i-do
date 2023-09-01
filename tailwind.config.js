44534f6215082b59d97e31419a1b6799fc4a603f
gaeun <knospe1@naver.com>
2023-09-01 10:44:15 +0900

    Fix: fix tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,tsx,ts}'],
+  theme:
+  {
+    extend: {
+      colors: {
+        'background': '#2B3A55',
+        'primary': '#CE7777',
+        'secondary': '#E8C4C4',
+        'textColor': '#F2E5E5',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
