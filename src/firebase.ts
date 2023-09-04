7e222c5c7e121d9096b107c5e31f6de1ed80da9c
gaeun <knospe1@naver.com>
2023-09-05 08:45:16 +0900

    Config: add firebase realtime database

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,6 +8,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
