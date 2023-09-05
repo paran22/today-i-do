b9c226214ec7d1330f5e2df2077ae77fc959761a
gaeun <knospe1@naver.com>
2023-09-05 14:20:17 +0900

    Fix: fix toDate

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,3 +1,5 @@
 export function toDate(dateTime: Date) {
-  return `${dateTime.getFullYear()}-${dateTime.getMonth()}-${dateTime.getDay()}`;
+  return `${dateTime.getFullYear()}-${
+    dateTime.getMonth() + 1
+  }-${dateTime.getDate()}`;
 }
