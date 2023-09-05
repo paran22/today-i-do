5dab74806337046b0500e563224d17300054bf29
gaeun <knospe1@naver.com>
2023-09-05 12:04:21 +0900

    Feat: add getBoardsByDate api

--- src/api/api.ts
+++ src/api/api.ts
@@ -25,3 +25,30 @@ export async function createBoard({ board, userId }: CreateBoardProps) {
     throw Error();
   }
 }
+interface GetBoardsByDateProps {
+  dateTime: Date;
+}
+
+interface BoardResponse {
+  id: string;
+  board: Board;
+}
+
+export async function getBoardsByDate({ dateTime }: GetBoardsByDateProps) {
+  const date = toDate(dateTime);
+  try {
+    const data = await get(ref(db, `${boardDbKey}/${date}`));
+    const boards = data.val();
+    if (!boards) return [];
+    return Object.values(boards)
+      .map((e) => Object.entries(e as Object))
+      .flat()
+      .map((e) => {
+        const item: BoardResponse = { id: e[0], board: e[1] };
+        return item;
+      });
+  } catch (e) {
+    console.error(e);
+    throw Error();
+  }
+}
