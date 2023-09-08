d14d021ad3ed9bad54bebc10d6dc42ae2887a348
gaeun <knospe1@naver.com>
2023-09-08 13:56:45 +0900

    Feat: add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import useAuthState from "./hooks/useAuthState";
 import useGoogleAuth from "./hooks/useGoogleAuth";
@@ -32,6 +33,7 @@ function App() {
       <section ref={ref} className="pt-12">
         <Outlet />
       </section>
+      <Footer />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bottom-0 flex w-screen justify-between items-center py-1 px-5 font-semibold text-sm">
+      <a href="https://github.com/paran22/today-i-do">GitHub</a>
+      <p className="">by GAEUN</p>
+    </footer>
+  );
+}
