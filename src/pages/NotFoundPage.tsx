126d53ee7e9283809759de60523578d6d69118bf
gaeun <knospe1@naver.com>
2023-09-08 14:13:15 +0900

    Feat: add NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { AiTwotoneNotification } from "react-icons/ai";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center gap-4 pt-36 ">
+        <AiTwotoneNotification className="text-6xl" />
+        <p className="text-3xl font-bold">잘못된 경로입니다.</p>
+      </div>
+      <Footer />
+    </>
+  );
 }
