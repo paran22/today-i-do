997364ec563a786ccc3b05e35ad85a4f6869605b
gaeun <knospe1@naver.com>
2023-09-08 16:53:57 +0900

    Feat: add to show trash icon when board is mine in Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,28 @@
 import { BoardModel } from "../model/boardModel";
+import { PiNotepadDuotone } from "react-icons/pi";
+import { FaRegTrashAlt } from "react-icons/fa";
+import useAuthState from "../hooks/useAuthState";
 
 export default function Board({ board }: { board: BoardModel }) {
-  const { todayDone, good, notGood, username } = board;
+  const { todayDone, good, notGood, username, userId } = board;
+  const { user } = useAuthState();
+  const isMine = user?.uid === userId;
+
   return (
     <section className="flex flex-col text-slate-700 bg-secondary rounded-lg py-2 px-2">
-      <div className="border-b-2 px-1 py-1">
-        <p>{todayDone}</p>
+      <div className="px-1 py-1 flex flex-col items-center gap-2">
+        <PiNotepadDuotone className="text-4xl" />
+        <p className="text-xl italic">{todayDone}</p>
       </div>
       <div className="py-1 px-1">
         <p>👍 {good}</p>
         <p>😭 {notGood}</p>
-        <p className="pt-2 px-2 text-end italic">{`by. ${username}`}</p>
+      </div>
+      <div className="flex justify-between pt-2 px-2 ">
+        <button>
+          <FaRegTrashAlt className={`${isMine ? "visible" : "hidden"}`} />
+        </button>
+        <p className="italic">{`by. ${username}`}</p>
       </div>
     </section>
   );
