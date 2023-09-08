dc8dea7b7fed6ca9668c3b8ce480e7032a3628aa
gaeun <knospe1@naver.com>
2023-09-08 13:23:48 +0900

    Fix: fix style of Header and Logo according to scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+import useScroll from "../hooks/useScroll";
 
 export default function Header() {
+  const { isScroll } = useScroll();
+  const backgroundColor = isScroll ? "bg-primary" : "bg-background";
   return (
-    <header className="flex px-2 py-2 justify-between items-end">
+    <header
+      className={`flex px-2 justify-between items-center fixed w-full h-12 ${backgroundColor} `}
+    >
       <Logo />
       <NavBar />
     </header>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import { PiNotepadDuotone } from "react-icons/pi";
 import useRoute from "../hooks/useRoute";
+import useScroll from "../hooks/useScroll";
 
 export default function Logo() {
+  const { isScroll } = useScroll();
   const { navigateToHome } = useRoute();
+  const textColor = isScroll ? "text-white" : "text-primary";
   return (
     <button
-      className="flex text-primary gap-2 text-4xl font-bold items-center"
+      className={`flex ${textColor} gap-2 text-4xl font-bold items-center`}
       onClick={navigateToHome}
     >
       <PiNotepadDuotone />
