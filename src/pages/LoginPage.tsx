e3ed7f336b73b06c057370c9513ed072b93251ad
gaeun <knospe1@naver.com>
2023-09-11 15:28:53 +0900

    Feat: add Loading in LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,25 +3,33 @@ import useGoogleAuth from "../hooks/useGoogleAuth";
 import useRoute from "../hooks/useRoute";
 import Button from "../components/Button";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import Loading from "../components/Loading";
 
 export default function LoginPage() {
   const { signInWithGoogle } = useGoogleAuth();
   const { navigateToPath, navigateToHome } = useRoute();
   const { state } = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = () => {
+    setIsLoading(true);
     signInWithGoogle()
       .then(() => (state ? navigateToPath(state) : navigateToHome()))
-      .catch(() => alert("에러가 발생했습니다."));
+      .catch(() => alert("에러가 발생했습니다."))
+      .finally(() => setIsLoading(false));
   };
   return (
-    <section className="flex flex-col gap-10 justify-start pt-20 items-center h-screen">
-      <p className="font-bold text-3xl">로그인</p>
-      <Button
-        icon={<FcGoogle />}
-        name="구글로 로그인하기"
-        onClick={handleClick}
-      />
-    </section>
+    <>
+      <section className="flex flex-col gap-10 justify-start pt-20 items-center h-screen">
+        <p className="font-bold text-3xl">로그인</p>
+        <Button
+          icon={<FcGoogle />}
+          name="구글로 로그인하기"
+          onClick={handleClick}
+        />
+      </section>
+      <Loading visible={isLoading} />
+    </>
   );
 }
